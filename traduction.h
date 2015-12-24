434ac4b8071297743fa1948d1456784b3d9ca06f
nathan07 <nathan.prinsac@iut-valence.fr>
2015-12-24 11:49:08 +0100

    Create traduction.h

---
+++ traduction.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SPECIAL "000000" /*champ SPECIAL dans la doc*/
+
+void trad(char tab[]);
+void tradOpEnBin(char *tab, char* tabOpEnBin);
+void concatenerAuDebut(char* dest, char* src);
